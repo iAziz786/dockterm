08d94263d47a56e3a56788b0695491b80eb16b94
Mohammad Aziz <mdaziz067@gmail.com>
2025-09-21 04:59:53 +0000

    Add lazygit

---
+++ migrations/user/1758430592_install-lazygit.sh.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Migration: install-lazygit.sh
+# Type: user
+# Created: Sun Sep 21 04:56:32 UTC 2025
+
+echo "Running migration: install-lazygit.sh"
+
+#############################################
+# Add your migration commands below
+#############################################
+
+sudo apt install lazygit
+
+#############################################
+# End of migration commands
+#############################################
+
+echo "✓ Migration completed: install-lazygit.sh"
